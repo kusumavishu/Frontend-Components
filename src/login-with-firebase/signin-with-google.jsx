e669072f141d377898d0952401b75c7253073c64
kusumavishu <vishukusuma2003@gmail.com>
2025-05-18 17:59:26 +0530

    GOOGLE SIGN-IN

--- src/login-with-firebase/signin-with-google.jsx
+++ src/login-with-firebase/signin-with-google.jsx
@@ -37,33 +37,6 @@ const SigninWithGoogle = () => {
     return subscriber; // unsubscribe on unmount
   }, []);
 
-  async function onFacebookButtonPress() {
-    // Attempt login with permissions
-    const result = await LoginManager.logInWithPermissions([
-      "public_profile",
-      "email",
-    ]);
-
-    if (result.isCancelled) {
-      throw "User cancelled the login process";
-    }
-
-    // Once signed in, get the users AccessToken
-    const data = await AccessToken.getCurrentAccessToken();
-
-    if (!data) {
-      throw "Something went wrong obtaining access token";
-    }
-
-    // Create a Firebase credential with the AccessToken
-    const facebookCredential = FacebookAuthProvider.credential(
-      data.accessToken
-    );
-
-    // Sign-in the user with the credential
-    return signInWithCredential(getAuth(), facebookCredential);
-  }
-
   async function onGoogleButtonPress() {
     // Check if your device supports Google Play
     await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
@@ -126,16 +99,6 @@ const SigninWithGoogle = () => {
                 console.log("Signed in with Google!")
               )
             }
-          />
-          <View style={{ height: 10 }} />
-          <Button
-            title="Facebook Sign-In"
-
-            // onPress={() =>
-            //   onFacebookButtonPress().then(() =>
-            //     console.log("Signed in with Facebook!")
-            //   )
-            // }
           />
         </>
       )}
